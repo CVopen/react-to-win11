ebfba4b183f83c4d2485a23b6847d01f320f1ba8
CVopen <1766730733@qq.com>
2022-06-10 00:33:34 +0800

    test(package.json): homepage

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,7 @@ const App: React.FC = () => {
       <h1>win11 to react</h1>
       <h2>test</h2>
       <Title>12</Title>
-
-      {count}
+      <div>{count}</div>
       <button onClick={() => dispatch(addCount(1))}>add</button>
     </>
   )
